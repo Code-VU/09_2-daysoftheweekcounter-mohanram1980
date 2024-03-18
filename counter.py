ac3ad97aaeda329232e7076dfd06b463402487d3
mohanram1980 <mohan.ram@veteransunited.com>
2024-03-18 10:08:46 -0500

    Count emails on the days they were sent on

--- counter.py
+++ counter.py
@@ -1,6 +1,23 @@
 def countDayOfTheWeek():
-    # This first line is provided for you
-    file_name = input("Enter a file name: ")
+    fname = input('Enter a file name: ')
+    try:
+        fhand = open(fname)
+    except FileNotFoundError:
+        print('File cannot be opened:', fname)
+        exit()
+
+    dictionary_days = dict()
+    for line in fhand:
+        words = line.split()
+        if len(words) < 3 or words[0] != 'From':
+            continue
+        else:
+            if words[2] not in dictionary_days:
+                dictionary_days[words[2]] = 1
+            else:
+                dictionary_days[words[2]] += 1
+
+    print(dictionary_days)
 
 
 
